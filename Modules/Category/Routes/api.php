73cfa22705a6ba2974c9641fe393bcb919885e9f
mahedi250 <bmaruf250@gmail.com>
2022-06-13 17:51:21 +0600

    auth added

--- Modules/Category/Routes/api.php
+++ Modules/Category/Routes/api.php
@@ -19,10 +19,10 @@ use Modules\Category\Http\Controllers\CategoryController;
 //     return $request->user();
 // });
 
-Route::group(['prefix' => 'category', 'as' => 'category.'], function () {
+Route::group(['prefix' => 'category', 'as' => 'category.', 'middleware' => ['auth:sanctum']], function () {
     Route::get('/parent', [CategoryController::class, 'index'])->name('all');
     Route::get('/child/{parent_id}', [CategoryController::class, 'getchild'])->name('all');
     Route::post('/store', [CategoryController::class, 'store'])->name('store');
-    Route::post('/update/{id}', [CategoryController::class, 'update'])->name('update');
+    Route::post('/ ', [CategoryController::class, 'update'])->name('update');
     Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('destroy');
 });
